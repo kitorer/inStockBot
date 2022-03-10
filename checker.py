2dfa3453c390e5cf2aa89b83a8c728fc357d7a8d
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-03-10 12:39:53 -0500

    BB LINKS
    returns BB links from in stock and ready to pick up filters

--- checker.py
+++ checker.py
@@ -3,7 +3,9 @@ import smtplib
 from time import sleep
 from datetime import datetime
 from email.message import EmailMessage
+from zoneinfo import available_timezones
 import mouse
+import bs4
 
 import selenium
 from selenium import webdriver
@@ -11,23 +13,22 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-url = "https://www.bestbuy.com"
-#debug lines
-#options = webdriver.ChromeOptions()
-#options.add_experimental_option("detach", True) 
+# yeah this probably relies on cookies working
+url = "https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&qp=soldout_facet%3DAvailability~Exclude%20Out%20of%20Stock%20Items%5Estorepickupstores_facet%3DStore%20Availability%20-%20In%20Store%20Pickup~571&st=graphics+card"
+
+# debug lines
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True) 
 
 s = Service("C:\Program Files (x86)\chromedriver.exe")
 driver = webdriver.Chrome(service = s) 
-#debug
-#driver = webdriver.Chrome(options=options)
+# debug
+# driver = webdriver.Chrome(options=options)
 
 log = ""
-# Boolean: returns true if word is found in the url
-def isInStock(url, word):
-    print("hellow")
 
-#using best buy search bar
-#returns a boolean
+# using best buy search bar
+# returns a boolean
 def searchbar(item_to_search):
     try:
         searchbar = driver.find_element(By.ID, "gh-search-input")
@@ -38,30 +39,39 @@ def searchbar(item_to_search):
     except:
         return False
 
-def searchFilters():
-    driver.find_element(By.ID, "store-pickup-pickuptoday").click
-    sleep(2)
-    driver.find_element(By.ID, "soldout_facet-Exclude-Out-of-Stock-Items-0").click
-    sleep(2)
+# has to be on the searched page 
+# ToDo: get it print where which item is available
+# the fulfillment comments were attempts for location tagging
+def isAvailable():
+    html = driver.page_source
+    soup = bs4.BeautifulSoup(html,"html.parser")
 
-    print("this worked")
+    items = soup.find_all("li", {"class": "sku-item"})
+
+    for product in items:
+        hrefs = product.find(href = True)
+        #fulfillmentID = product.find(By.ID,"fulfillment-fulfillment-summary")
+        #fulfillmentText = fulfillmentID.find("strong")
+        link = "https://www.bestbuy.com" + hrefs['href']
+        return link
+
+        #print(fulfillmentID['strong'])
+        
+# ping every 5 minutes or something
+# call notify if there is a new item that passes in the new items
+def checker():
+    linkStorage = []
+    print()
+
+def notify():
+    print()
 
 def main():
     driver.get(url)
-
-    #dont miss out on the latest deals and more
-    sleep(5)
-    print("5s has passed")
-    mouse.click()
-
-    sleep(3)
-    print("3s has passed")
-    searchbar("graphics cards")
-
-    sleep(3)
-    searchFilters()
-
     print(driver.title)
+    sleep(2)
+    isAvailable()
+    driver.close()
 
 if __name__ == "__main__":
     main()
