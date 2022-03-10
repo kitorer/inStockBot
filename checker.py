7a9a6f0dad122f9573dca479f6c5bc8b67bbce18
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-03-09 22:15:22 -0500

    search worked
    however checkboxes dont :(

--- checker.py
+++ checker.py
@@ -1,37 +1,67 @@
 import json
-from posixpath import abspath
 import smtplib
-import urllib.request
+from time import sleep
 from datetime import datetime
-from bs4 import BeautifulSoup
 from email.message import EmailMessage
+import mouse
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+url = "https://www.bestbuy.com"
+#debug lines
+#options = webdriver.ChromeOptions()
+#options.add_experimental_option("detach", True) 
+
+s = Service("C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(service = s) 
+#debug
+#driver = webdriver.Chrome(options=options)
 
 log = ""
-
 # Boolean: returns true if word is found in the url
 def isInStock(url, word):
-    global log
-    try:
-        page = urllib.request.urlopen(url)
-        soup = BeautifulSoup(page, features= 'html.parser')
-        print(page.read())
+    print("hellow")
 
-        if word in soup.text:
-            return True
-        return False
+#using best buy search bar
+#returns a boolean
+def searchbar(item_to_search):
+    try:
+        searchbar = driver.find_element(By.ID, "gh-search-input")
+        searchbar.send_keys(item_to_search)
+        searchbar.send_keys(Keys.RETURN)
+
+        return True
     except:
-        log += "Error parsing url"
+        return False
+
+def searchFilters():
+    driver.find_element(By.ID, "store-pickup-pickuptoday").click
+    sleep(2)
+    driver.find_element(By.ID, "soldout_facet-Exclude-Out-of-Stock-Items-0").click
+    sleep(2)
+
+    print("this worked")
 
 def main():
-    print("This works on non js sites")
-    url = input("site to check ")
-    keyword = input("item ")
-    available = isInStock(url,keyword)
+    driver.get(url)
 
-    if available:
-        print("found!")
-    else:
-        print("not found!")
+    #dont miss out on the latest deals and more
+    sleep(5)
+    print("5s has passed")
+    mouse.click()
+
+    sleep(3)
+    print("3s has passed")
+    searchbar("graphics cards")
+
+    sleep(3)
+    searchFilters()
+
+    print(driver.title)
 
 if __name__ == "__main__":
     main()
